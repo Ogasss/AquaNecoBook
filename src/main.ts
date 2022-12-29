17d20b6bdc3f537e2486071a379e0754f81ad5e7
Ogas <2309704992@qq.com>
2022-12-29 15:08:22 +0800

    尝试提交

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,27 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
+import { http } from './shared/Http';
 
 const router = createRouter({ history, routes })
+
+router.beforeEach(async (to, from)=> {
+    if(to.path === '/' || to.path === '/welcome' || to.path.startsWith('/sign_in')){
+        return true
+    }else{
+        await http.get('/me').catch(() => {
+         return '/sign_in?return_to='+ to.path
+        })
+        return true
+    }
+    // beforeEnter: async (to, from, next) => {
+    //   await http.get('/me').catch(() => {
+    //     next('/sign_in?return_to='+ to.path)
+    //   })
+    //   next()
+    // },  
+})
+
 
 const app = createApp(App)
 app.use(router)
