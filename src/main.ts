a29f41b93e4d04d5126ab05d448ca4bb757b94db
Ogas <83815549+Unclotho@users.noreply.github.com>
2023-03-12 20:23:35 +0800

    加强了用户认证的检测，修复了首次打开重定向welcome页失败的bug

--- src/main.ts
+++ src/main.ts
@@ -9,30 +9,37 @@ import { mePromise, fetchMe } from './shared/me';
 const router = createRouter({ history, routes })
 
 export const me = ref<User>()
-
 fetchMe().then(
         async ()=>{
             const response = await mePromise
             me.value = response?.data?.resource
-            const jwt = localStorage.getItem('jwt')
-            if(me.value === undefined && jwt !== null){
-                localStorage.removeItem('jwt')
-            }
+            // const jwt = localStorage.getItem('jwt')
+            // if(me.value === undefined && jwt !== null){
+            //     localStorage.removeItem('jwt')
+            // }
         },
     )
 
-router.beforeEach(async (to, from)=> {
+router.beforeEach((to, from)=> {
     // to.path === '/' || to.path === '/welcome'  || to.path === '/start' || to.path.startsWith('/sign_in')
+    const jwt = localStorage.getItem('jwt')
+    if(me.value === undefined && jwt !== null){
+        localStorage.removeItem('jwt')
+    }
     if(to.path === '/' || to.path === '/welcome'  || to.path.startsWith('/sign_in')){
+        // alert(from.path+to.path)
         return true
     }else{
-        const jwt = localStorage.getItem('jwt')
-        if(jwt === null){
-            return 'sign_in'
+        if(jwt === null || me.value === undefined){
+            // alert(from.path+to.path)
+            if(from.path === '/welcome') {
+                return 'sign_in'
+            }else{
+                return '/welcome'
+            }
         }else{
             return true
         }
-        
     }
 })
 
